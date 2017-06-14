fa85199348250b6cbe1d0233b2bd300970b63376
DESKTOP-AF5A5H4\dongkyu <kdk8747@naver.com>
2017-06-14 16:56:02 +0900

    added pm2 log. and fixed configuration error

--- server/server.js
+++ server/server.js
@@ -26,6 +26,11 @@ app.get('*.js', function (request, response) {
 });
 
 
+app.get('/crash', function(request, response) {
+  exit(9999);
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
