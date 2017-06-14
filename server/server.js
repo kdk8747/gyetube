fdc7a38a892d106e949a81ccabcef9530a488d3b
DESKTOP-AF5A5H4\dongkyu <kdk8747@naver.com>
2017-06-14 15:30:44 +0900

    (3)fixed compression-webpack-plugin server-side code (ready-made gzip)

--- server/server.js
+++ server/server.js
@@ -1,45 +1,30 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/../dist'));
-app.use(express.static(__dirname + '/..'));
+var __public = path.resolve(__dirname + '/../dist');
 
 
 app.get('/', function(request, response) {
-    response.sendFile('index.html');
+  console.log(__public + '/index.html');
+  response.sendFile(__public + '/index.html');
 });
 
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+
+app.get('*.js', function (request, response) {
+  if (request.url.substr(1,3) == 'app') {
+    request.url = __public + request.url;
+  }
+  else {
+    request.url = __public + request.url + '.gz';
+    response.set('Content-Encoding', 'gzip');
+  }
+  console.log(request.url);
+  response.sendFile(request.url);
 });
 
-app.get('*.js', function (request, response, next) {
-  request.url = request.url + '.gz';
-  response.set('Content-Encoding', 'gzip');
-  next();
-});
-
-app.get('/db', function (request, response) {
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
-
-    connection.connect();
-
-    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-        if (err) throw err;
-
-        console.log('The solution is: ', rows[0].solution);
-    });
-
-    connection.end();
-    response.send('good');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
