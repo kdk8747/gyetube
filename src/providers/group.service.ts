462fc7d65c6596fd6a52edaded359743e0d5f75e
DESKTOP-AF5A5H4\dongkyu <kdk8747@naver.com>
2017-11-01 09:59:56 +0900

    added group service cache

--- src/providers/group.service.ts
+++ src/providers/group.service.ts
@@ -1,33 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Headers } from '@angular/http';
-
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/take';
-
 import { Group } from '../models';
 import { AuthHttp } from 'angular2-jwt';
 import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/publishLast';
+
 
 @Injectable()
 export class GroupService {
   private groupsUrl = '/api/v1.0/groups';  // URL to web api
   private headers = new Headers({ 'Content-Type': 'application/json' });
 
+  private groups = {};
+
   constructor(
     public http: AuthHttp
   ) { }
 
+  print() {
+    console.log('groups: ');
+    for (let elem in this.groups)
+      console.log(elem);
+  }
+
   getGroups(): Observable<Group[]> {
     return this.http.get(this.groupsUrl)
-    .map(response => response.json() as Group[])
-    .take(1);
+      .map(response => response.json() as Group[])
+      .take(1);
   }
 
   getGroup(id: string): Observable<Group> {
     const url = `${this.groupsUrl}/${id}`;
-    return this.http.get(url)
-    .map(response => response.json() as Group)
-    .take(1);
+
+    if (!this.groups[id])
+      this.groups[id] = this.http.get(url)
+        .map(response => response.json() as Group)
+        .publishLast().refCount();
+    return this.groups[id];
   }
 
   update(user: Group): Observable<Group> {
@@ -40,7 +52,7 @@ export class GroupService {
 
   create(user: Group): Observable<Group> {
     return this.http
-      .post(this.groupsUrl, JSON.stringify(user),{ headers: this.headers })
+      .post(this.groupsUrl, JSON.stringify(user), { headers: this.headers })
       .map(response => response.json() as Group)
       .take(1);
   }
