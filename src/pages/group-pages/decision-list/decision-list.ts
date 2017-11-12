19c0429718b40422af598e3a2cc4f2425eb38898
DESKTOP-AF5A5H4\dongkyu <kdk8747@naver.com>
2017-11-12 15:35:35 +0900

    added timelineMode (TODO: server side)

--- src/pages/group-pages/decision-list/decision-list.ts
+++ src/pages/group-pages/decision-list/decision-list.ts
@@ -14,6 +14,7 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: 'decision-list.html',
 })
 export class DecisionListPage {
+  timelineMode: boolean = false;
 
   groupId: string;
   decisions: Observable<Decision[]>;
