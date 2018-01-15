47580f9daa8c8df6f12f235b0e29124cccc3ffed
DESKTOP-AF5A5H4\dongkyu <kdk8747@naver.com>
2018-01-15 15:50:26 +0900

    debug amazon s3

--- src/app/environment-variables/production.ts
+++ src/app/environment-variables/production.ts
@@ -1,6 +1,6 @@
 export const prodVariables = {
   apiEndpoint: 'https://gyetube.com',
-  amazonS3Endpoint: 'http://gyetube-groups.s3.ap-northeast-2.amazonaws.com/',
+  amazonS3Endpoint: 'http://gyetube-groups.s3.amazonaws.com/',
   environmentName: 'Production Environment',
   ionicEnvName: 'prod'
 };
